d8afb87024f5378e0953fd32c9ed24364648f874
silverantos <silverantos@gmail.com>
2026-09-18 13:19:24 +0100

    Add SystemEndpoints extension class with System, Preference Parameters and Licensing routes

---
+++ Endpoints/SystemEndpoints.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace LogicPOS.ApiServer.Endpoints;
+
+public static class SystemEndpoints
+{
+    public static void MapSystemEndpoints(this IEndpointRouteBuilder app)
+    {
+        var system = app.MapGroup("/system")
+            .WithTags("System");
+
+        system.MapGet("/api-version", () => Results.Ok("1.5.2 retail"))
+            .WithName("GetApiVersion");
+
+        system.MapGet("/information", () => Results.Ok(new
+        {
+            Culture = "pt-PT",
+            Country = "pt",
+            DatabaseModule = "SQLite"
+        }))
+        .WithName("GetSystemInformation");
+
+        var preferenceParameters = app.MapGroup("/preference-parameters")
+            .WithTags("Preference Parameters");
+
+        preferenceParameters.MapGet("", () => Results.Ok(Array.Empty<object>()))
+            .WithName("GetPreferenceParameters");
+
+        var licensing = app.MapGroup("/licensing")
+            .WithTags("System");
+
+        licensing.MapGet("/data", () => Results.Ok(new
+        {
+            status = "Active"
+        }))
+        .WithName("GetLicensingData");
+
+        licensing.MapGet("/hardware-id", () => Results.Ok(new
+        {
+            hardwareId = "LINUX-LOCAL"
+        }))
+        .WithName("GetLicensingHardwareId");
+    }
+}
